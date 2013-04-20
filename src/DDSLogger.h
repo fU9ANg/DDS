2a8552a13cbccf3c66d60ee0fe3f4d3b605662d9
fU9ANg <bb.newlife@gmail.com>
2013-04-20 09:07:29 +0800

    adding logger class for logging

---
+++ src/DDSLogger.h
@@ -0,0 +1,70 @@
+
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <strings.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+
+namespace DDS
+{
+
+namespace sharedLib
+{
+
+class DDSLogger
+{
+
+    public:
+        enum eLevel
+        {
+            TRACE,
+            DEBUG,
+            INFO,
+            WARN,
+            ERROR,
+            FATAL,
+            NUMBER_OF_LOGTYPE,
+        };
+
+        DDSLogger (const char* file, int line);
+        DDSLogger (const char* file, int line, eLevel lvl);
+        DDSLogger (const char* file, int line, eLevel lvl, const char* func);
+        DDSLogger (const char* file, int line, bool abort);
+
+        ~DDSLogger (void);
+
+        std::ostream& stream();
+
+        static eLevel Level (void);
+        static void setLevel (eLevel lvl);
+
+};
+
+#define LOG_TRACE if (DDSLogger::Level() <= DDSLogger::TRACE) \
+                      DDSLogger(__FILE__, __LINE__, DDSLogger::TRACE, __func__).stream()
+#define LOG_DEBUG     DDSLogger(__FILE__, __LINE__, DDSLogger::DEBUG, __func__).stream()
+#define LOG_INFO      DDSLogger(__FILE__, __LINE__).stream()
+#define LOG_WARN      DDSLogger(__FILE__, __LINE__, DDSLogger::WARN).stream()
+#define LOG_ERROR     DDSLogger(__FILE__, __LINE__, DDSLogger::ERROR).stream()
+#define LOG_FATAL     DDSLogger(__FILE__, __LINE__, DDSLogger::FATAL).stream()
+#define LOG_SYSERR    DDSLogger(__FILE__, __LINE__, false).stream();
+#define LOG_SYSFATAL  DDSLogger(__FILE__, __LINE__, true).stream();
+
+#define CHECK_NOTNULL(val) \
+    ::DDS::sharedLib::CheckNotNULL(__FILE__, __LINE__, "'" #val "' Must be non NULL", (val))
+
+template <typename T>
+T* CheckNotNULL (const char* file, int line, const char* names, T* ptr)
+{
+    if (ptr == NULL) {
+        DDSLogger (file, line, DDSLogger::FATAL).stream() << names;
+    }
+
+    return ptr;
+}
+
+}
+}
