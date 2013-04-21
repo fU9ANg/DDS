a348819f44d76d5cf8fc58e867bd4cb85c55076c
fU9ANg <bb.newlife@gmail.com>
2013-04-21 22:52:45 +0800

    update build script file for make

--- src/build.sh
+++ src/build.sh
@@ -5,3 +5,7 @@ g++ -o DDSockets.o -c DDSockets.cc -I.
 
 echo "compiling ... DDSMutexLockGuard.o"
 g++ -o DDSMutexLockGuard.o -c DDSMutexLockGuard.cc -I.
+echo "compiling ... DDSCountDown.o"
+g++ -o DDSCountDown.o -c DDSCountDown.cc -I.
+echo "compiling ... DDSException.o"
+g++ -o DDSException.o -c DDSException.cc -I.
