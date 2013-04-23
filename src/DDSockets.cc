d6d371fec124fb74f29d1d4fe56a4c9883711c5e
fU9ANg <bb.newlife@gmail.com>
2013-04-24 01:37:43 +0800

    update sockets class

--- src/DDSockets.cc
+++ src/DDSockets.cc
@@ -1,6 +1,53 @@
 
 #include "DDSockets.h"
 
-DDSockets::DDSockets ()
+DDSockets::~DDSockets (void)
 {
+    DDSocketFunc::close (m_sockfd);
+}
+
+int DDSockets::getFd (void)
+{
+    return (m_sockfd);
+}
+
+void DDSockets::bindAddress (const DDSInetAddr& localAddr)
+{
+    DDSocketFunc::bindToAddr (m_sockfd, localAddr.getInetAddr ());
+}
+
+void DDSockets::listen (void)
+{
+    DDSocketFunc::listenQueue (m_sockfd);
+}
+
+int DDSockets::accpet (DDSInetAddr* peerAddr)
+{
+    struct sockaddr_in addr;
+    int connfd = DDSocketFunc::acceptConnect (m_sockfd, &addr);
+    (void) memset (&addr, 0x00, sizeof (addr));
+
+    if (connfd >= 0)
+        peeraddr->setInetAddr (addr);
+
+    return (connfd);
+}
+
+void DDSockets::shutdownWrite (void)
+{
+    DDSocketFunc::shutdownWrite (m_sockfd);
+}
+
+void DDSockets::setTcpNoDelay (bool on)
+{
+    int opt_val = on ? 1 : 0;
+    ::setsockopt (m_sockfd, IPPROTO_TCP, TCP_NODELAY, \
+                  &opt_val, sizeof (opt_val));
+}
+
+void DDSockets::setReuseAddr (bool on)
+{
+    int opt_val = on ? 1 : 0;
+    ::setsockopt (m_sockfd, SOL_SOCKET, SO_REUSEADDR, \
+                  &opt_val, sizeof (opt_val));
 }
--- src/DDSockets.h
+++ src/DDSockets.h
@@ -2,6 +2,8 @@
 #ifndef _DDS_NETWORK_DDSOCKETS_H
 #define _DDS_NETWORK_DDSOCKETS_H
 
+#include "DDSockets.h"
+
 namespace DDS
 {
 namespace NETWORK
@@ -10,7 +12,20 @@ namespace NETWORK
 class DDSockets
 {
     public:
+        explicit DDSockets (int sockFd)
+            : m_sockfd (sockFd)
+        {}
+        ~DDSockets (void);
+        int  getFd (void);
+        void bindAddress (const DDSInetAddr& localAddr);
+        void listen (void);
+        int  accpet (DDSInetAddr* peerAddr);
+        void shutdownWrite (void);
+        void setTcpNoDelay (bool on);
+        void setReuseAddr (bool on);
+
     private:
+        const int m_sockfd;
 };
 
 }
